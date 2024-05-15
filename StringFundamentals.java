683554d35aed0aed75fd9b52548ebe5d9d2ef899
zemanue <manureco.97@gmail.com>
2024-05-15 23:17:37 +0200

    Update StringFundamentals.java
    Add a updateLight() method, that simulates a traffic light that switches:
    - Green to yellow
    - Yellow to red
    - Red to green

--- StringFundamentals.java
+++ StringFundamentals.java
@@ -36,6 +36,19 @@ public class StringFundamentals {
         return String.join(" ", words);
     }
 
+    // Thinkful - Logic Drills: Traffic light
+    // You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+    // Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+    // For example, when the input is green, output should be yellow.
+    public static String updateLight(String current) {
+        switch (current) {
+            case "red": return "green";
+            case "yellow": return "red";
+            case "green": return "yellow";
+            default: throw new IllegalArgumentException();
+        }
+    }
+
     public static void main(String[] args) {
         String owner = "Jacob";
 
